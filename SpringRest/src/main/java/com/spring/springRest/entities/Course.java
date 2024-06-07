5076464f4596c9d7b15ae24f5934abe29ad55896
CR035858 <shekarchandru.5@gmail.com>
2024-06-07 11:39:07 +0530

    Add files via upload

---
+++ SpringRest/src/main/java/com/spring/springRest/entities/Course.java
@@ -0,0 +1,52 @@
+package com.spring.springRest.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+	
+	@Id
+	private int id;
+	private String title;
+	private String description;
+	
+	
+	
+	public Course() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", title=" + title + ", description=" + description + ", getId()=" + getId()
+				+ ", getTitle()=" + getTitle() + ", getDescription()=" + getDescription() + ", getClass()=" + getClass()
+				+ ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+	}
+	
+	public Course(int id, String title, String description) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
